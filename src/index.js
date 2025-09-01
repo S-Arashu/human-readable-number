ad49917a2eb08913275eee3a6c7be50ebd340881
S-Arashu <118382286+S-Arashu@users.noreply.github.com>
2025-09-01 16:23:50 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,65 @@
-module.exports = function toReadable(/* number */) {
-  throw new Error('Not implemented');
+module.exports = function toReadable(number) {
+  const numTillTwenty = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+  ];
+
+  const tens = [
+    '',
+    '',
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+  ];
+
+  if (number < 20) {
+    return numTillTwenty[number];
+  }
+  if (number < 100 && String(number).endsWith('0')) {
+    return tens[+String(number)[0]];
+  }
+  if (number < 100) {
+    return `${tens[+String(number)[0]]} ${numTillTwenty[+String(number)[1]]}`;
+  }
+  if (number < 1000 && String(number).endsWith('00')) {
+    return `${numTillTwenty[+String(number)[0]]} hundred`;
+  }
+  if (
+    number < 1000 &&
+    (String(number)[1] === '1' || String(number)[1] === '0')
+  ) {
+    return `${numTillTwenty[+String(number)[0]]} hundred ${
+      numTillTwenty[+(String(number)[1] + String(number)[2])]
+    }`;
+  }
+  if (number < 1000 && String(number).endsWith('0')) {
+    return `${numTillTwenty[+String(number)[0]]} hundred ${tens[+String(number)[1]]}`;
+  }
+  if (number < 1000) {
+    return `${numTillTwenty[+String(number)[0]]} hundred ${tens[+String(number)[1]]} ${numTillTwenty[+String(number)[2]]}`;
+  }
+  return `Wrong value`;
 };
